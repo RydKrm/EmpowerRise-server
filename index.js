cc1fd319a459575b7b756f1df9ade26218a70642
RydKrm <alriyadkarim@gmail.com>
2023-08-23 21:48:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+
+const adminRouter = require('./admin');
+const sellerRouter = require('./seller');
+const customerRouter = require('./customer');
+const productRouter = require('./product');
+const dashboardRouter = require('./dashboard');
+
+app.use(cors())
+app.use(express.json())
+
+  app.use('/',adminRouter);
+
+
+app.get('/', (req, res) => {
+    res.send('Empower Rise Website Running!')
+})
+
+app.listen(port, () => {
+    console.log(`Empower Rise Website listening on port ${port}`)
+})
