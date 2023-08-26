7e55c73ac09449e2c0aec2f2f9f16f50074ab448
Abu-Sahad <abusahad603@gmail.com>
2023-08-26 17:05:58 +0600

    data throw mongoDB from blog add page api create

--- users.js
+++ users.js
@@ -7,6 +7,7 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const usersCollection = client.db('empowerRise').collection('users')
+        const blogsCollection = client.db('empowerRise').collection('blogs')
         userRouter.route('/users')
             .post(async (req, res) => {
                 const user = req.body;
@@ -16,6 +17,17 @@ async function run() {
                     return res.send({ message: 'user already exists' })
                 }
                 const result = await usersCollection.insertOne(user);
+                res.send(result)
+            })
+        userRouter.route('/blogs')
+            .post(async (req, res) => {
+                const user = req.body;
+                const query = { email: user.email }
+                const existingUser = await usersCollection.findOne(query)
+                if (existingUser) {
+                    return res.send({ message: 'user already exists' })
+                }
+                const result = await blogsCollection.insertOne(user);
                 res.send(result)
             })
 
